409c15e0ae52b00d3d404bcd6a9150960592136e
duh-nicole <215088978+duh-nicole@users.noreply.github.com>
2026-04-08 20:25:42 -0500

    Modified existing code to fit assignment requirements by taking logic out of one large file and separating it out into the separate modules and services folders. Includes creating the init files so the folders actually worked as intended.

---
+++ main_controller.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from typing import List, Optional
+from modules.product_models import Products, ProductsRequest
+from services import game_service
+
+app = FastAPI(title='Module 5 API',
+              version='0.0.3',
+              contact={"name": 'Nicole Duhan', "email": 'nduhan@mail.mccneb.edu'},
+              description ='Assignment 5')
+
+@app.get("/products", response_model=List[Products])
+async def get_products():
+    return await game_service.get_all_games()
+
+@app.get("/product/{product_id}", response_model=Products)
+async def get_product(product_id: int):
+    game = await game_service.get_game_by_id(product_id)
+    return game
+
+## Search Endpoints ##
+@app.get("/products/price", response_model=List[Products])
+async def get_all_products_price_range(min_price: float, max_price: float):
+    return await game_service.get_games_by_price_range(min_price, max_price)
+
+@app.get("/products/search", response_model=List[Products])
+async def search_products(product_price: float, product_type: Optional[str] = None):
+    return await game_service.search_games(product_price, product_type)
+
+@app.post("/products/mod")
+async def update_products(products: ProductsRequest):
+    existing_game = await game_service.get_game_by_id(products.ID)
+
+    if existing_game:
+        await game_service.update_existing_game(products)
+        return "Modified Product!"
+    else:
+        await game_service.add_new_game(products)
+        return "Added New Product!"
+
