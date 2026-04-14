00c3b0d07d98abda42bb491cdab9acdcf7d7a18f
duh-nicole <215088978+duh-nicole@users.noreply.github.com>
2026-04-14 15:51:42 -0500

    Modified products mod endpoint to include error message for bad request object to include an empty string check in the POST method.

--- main_controller.py
+++ main_controller.py
@@ -13,35 +13,35 @@ app = FastAPI(title='Module 6 API',
 async def get_products():
     games = await game_service.get_all_games()
     if not games:
-        raise HTTPException(status_code=400, detail="No entries exist!")
+        raise HTTPException(status_code=400, detail=StatusMessage(content="No entries exist!").dict())
     return games
 
 @app.get("/product/{product_id}", response_model=Products, responses={400: {"model": StatusMessage}})
 async def get_product(product_id: int):
     game = await game_service.get_game_by_id(product_id)
     if not game:
-        raise HTTPException(status_code=400, detail="No entry was found matching your query.")
+        raise HTTPException(status_code=400, detail=StatusMessage(content="No entry was found matching your query.").dict())
     return game
 
 @app.get("/products/price", response_model=List[Products], responses={400: {"model": StatusMessage}})
 async def get_all_products_price_range(min_price: float, max_price: float):
     results = await game_service.get_games_by_price_range(min_price, max_price)
     if not results:
-        raise HTTPException(status_code=400, detail="No products were found matching your criteria!")
+        raise HTTPException(status_code=400, detail=StatusMessage(content="No products were found matching your min/max price criteria!").dict())
     return results
 
 @app.get("/products/search", response_model=List[Products], responses={400: {"model": StatusMessage}})
 async def search_products(product_price: float, product_type: Optional[str] = None):
     results = await game_service.search_games(product_price, product_type)
     if not results:
-        raise HTTPException(status_code=400, detail="No products were found from the price or type given!")
+        raise HTTPException(status_code=400, detail=StatusMessage(content="No products were found from the price or type given!").dict())
     return results
 
 @app.post("/products/mod", responses={400: {"model": StatusMessage}})
 async def update_products(products: ProductsRequest):
     ## Checking if required strings are empty
     if products.Name == "" or products.Type == "":
-        raise HTTPException(status_code=400, detail="The Request Object was not sent properly!")
+        raise HTTPException(status_code=400, detail=StatusMessage(content="The Request Object was not sent properly!").dict())
     existing_game = await game_service.get_game_by_id(products.ID)
     if existing_game:
         await game_service.update_existing_game(products)
