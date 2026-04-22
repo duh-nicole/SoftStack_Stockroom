13e3f078ebc76a1627015f61bcda47f9e0960376
duh-nicole <215088978+duh-nicole@users.noreply.github.com>
2026-04-22 17:37:15 -0500

    Finished unit tests for /products/mod. Updated main_controller to reflect message changes for better clarity.
    All unit tests passed and verified.

--- test_main.py
+++ test_main.py
@@ -11,10 +11,10 @@ def test_get_products_success():
 
 # Testing /product/{product_id} - Success
 def test_get_product_id_success():
-    response = client.get("/product/1")
+    response = client.get("/product/5")
     assert response.status_code == 200
 
-# Testing /product/{product_id} Fail - 400 Bad Request
+# Testing /product/{product_id} - Fail
 def test_get_product_id_not_found():
     response = client.get("/product/9999")
     assert response.status_code == 400
@@ -24,7 +24,7 @@ def test_get_product_id_not_found():
 
 # Testing /product/price - Success
 def test_get_product_price_range_success():
-    response = client.get("/products/price?min_price=10.0&max_price=100.0")
+    response = client.get("/products/price?min_price=14.00&max_price=16.00")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
@@ -38,15 +38,54 @@ def test_get_product_price_range_not_found():
 
 # Testing /products/search - Success
 def test_get_games_search_success():
-    response = client.get("/games")
+    response = client.get("/products/search?product_price=59.99")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
 
 # Testing /products/search - Fail
 def test_get_games_search_not_found():
-    response = client.get("/games")
+    response = client.get("/products/search?product_price=0.05")
     assert response.status_code == 400
     actual_message = response.json()["detail"]["content"]
     assert actual_message == "Whoops! No games meet your type and price."
 
-# Testing /products/mod - Success
+
+# Testing /products/mod
+# Test - Validation Empty Strings - Fail
+def test_update_product_invalid_data():
+    payload = {
+        "ID": 1,
+        "Name": "",
+        "Price": 19.99,
+        "Type": "RPG"
+    }
+    response = client.post("/products/mod", json=payload)
+    assert response.status_code == 400
+    assert response.json()["detail"]["content"] == "Product type or name cannot be left empty!"
+
+
+# Test - Modification of Existing ID
+def test_update_products_modify_existing():
+    payload = {
+        "ID": 3,
+        "Name": "Minecraft Java Edition",
+        "Price": 29.99,
+        "Type": "Mojang"
+    }
+    response = client.post("/products/mod", json=payload)
+    assert response.status_code == 200
+    assert response.json() == "Product has been successfully modified!"
+
+
+
+# Test - Adding New Product (New ID)
+def test_update_products_add_new():
+    payload = {
+        "ID": 1234,
+        "Name": "Sims 3",
+        "Price": 29.99,
+        "Type": "EA"
+    }
+    response = client.post("/products/mod", json=payload)
+    assert response.status_code == 200
+    assert response.json() == "Product has been successfully added!"
