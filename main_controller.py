2fdf7a99766209bfea3af6e83640e88b9e5c9641
duh-nicole <215088978+duh-nicole@users.noreply.github.com>
2026-04-25 15:40:24 -0500

    Merged Assignment 7 with Main. Created Assignment 8 branch.

--- main_controller.py
+++ main_controller.py
@@ -20,33 +20,33 @@ async def get_products():
 async def get_product(product_id: int):
     game = await game_service.get_game_by_id(product_id)
     if not game:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=StatusMessage(content="No entry was found matching your query.").dict())
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=StatusMessage(content="Whoops! The product_id was not found.").dict())
     return game
 
 @app.get("/products/price", response_model=List[Products], responses={400: {"model": StatusMessage}})
 async def get_all_products_price_range(min_price: float, max_price: float):
     results = await game_service.get_games_by_price_range(min_price, max_price)
     if not results:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=StatusMessage(content="No products were found matching your min/max price criteria!").dict())
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=StatusMessage(content="Whoops! No products within the specified range.").dict())
     return results
 
 @app.get("/products/search", response_model=List[Products], responses={400: {"model": StatusMessage}})
 async def search_products(product_price: float, product_type: Optional[str] = None):
     results = await game_service.search_games(product_price, product_type)
     if not results:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=StatusMessage(content="No products were found from the price or type given!").dict())
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=StatusMessage(content="Whoops! No games meet your type and price.").dict())
     return results
 
 @app.post("/products/mod", responses={400: {"model": StatusMessage}})
 async def update_products(products: ProductsRequest):
     ## Checking if required strings are empty
     if products.Name == "" or products.Type == "":
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=StatusMessage(content="The Request Object was not sent properly!").dict())
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=StatusMessage(content="Product type or name cannot be left empty!").dict())
     existing_game = await game_service.get_game_by_id(products.ID)
     if existing_game:
         await game_service.update_existing_game(products)
-        return "Modified Product!"
+        return "Product has been successfully modified!"
     else:
         await game_service.add_new_game(products)
-        return "Added New Product!"
+        return "Product has been successfully added!"
 
